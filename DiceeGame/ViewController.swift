a02d7600322fb6d83953ba558596b452177b66eb
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-07-23 11:23:53 +0500

    Dice Game marathon 8.0

--- DiceeGame/ViewController.swift
+++ DiceeGame/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var diceeImageViewOne: UIImageView!
+    @IBOutlet var diceeImageViewTwo: UIImageView!
+    
+    var leftDiceeNumber = 1
+    var rightDiceeNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonTapped(_ sender: Any) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+                
+        diceeImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        diceeImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+        
+        
+    }
+    
 }
 
